f77a1dc010615543696355eb0a91afbd4a2b7dc5
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-05-29 12:26:43 +0330

    INHERITANCE IN CLASSES

---
+++ animals.js
@@ -0,0 +1,23 @@
+class Animal {
+  constructor(name) {
+    this.name = name
+  }
+
+  speak() {
+    return this.name + ' SOUNDS'
+  }
+}
+
+class Dog extends Animal {
+  constructor(name) {
+    super(name);
+  }
+  bark() {
+    return this.name + ' BARKS'
+  }
+}
+
+const myDog = new Dog('JACKIE')
+console.log(myDog.speak())
+console.log(myDog.bark())
+
