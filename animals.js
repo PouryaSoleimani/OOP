23a6dc7ce5ce1c919e0434c9604202fd65a905b9
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-05-29 13:24:17 +0330

    THIS

--- animals.js
+++ animals.js
@@ -1,56 +1,60 @@
 class Animal {
-
   constructor(name) {
-    this.name = name
+    this.name = name;
   }
 
   speak() {
-    return this.name + ' SOUNDS'
+    return this.name + " SOUNDS";
   }
-
 }
 
 class Dog extends Animal {
-
   constructor(name) {
     super(name);
   }
 
   bark() {
-    return this.name + ' BARKS'
+    return this.name + " BARKS";
   }
-
 }
 
-const myDog = new Dog('JACKIE')
+const myDog = new Dog("JACKIE");
 
-console.log(myDog.speak())
-console.log(myDog.bark())
+console.log(myDog.speak());
+console.log(myDog.bark());
 
 class User {
   constructor(username, email) {
-    this.username = username
-    this.email = email
+    this.username = username;
+    this.email = email;
   }
 
   static validateUser(username, email) {
-    if (username.trim() == '' || username.length < 4 || email.trim() == '' || !email.includes("@")) {
-      console.log('%c 🛑 USER INFOS ARE NOT VALID ...', 'color :rgb(248, 120, 120)');
+    if (username.trim() == "" || username.length < 4 || email.trim() == "" || !email.includes("@")) {
+      console.log("%c 🛑 USER INFOS ARE NOT VALID ...", "color :rgb(248, 120, 120)",);
       return false;
     }
-    console.log('%c ✅ USER INFOS ARE VALID ...', 'color :rgb(120, 248, 152)');
-    return true
+    console.log("%c ✅ USER INFOS ARE VALID ...", "color :rgb(120, 248, 152)");
+    return true;
   }
 }
 
-const username = 'pourya'
-const email = 'pourya@gmail.com'
+const username = "pourya";
+const email = "pourya@gmail.com";
 
-const isValid = User.validateUser(username, email)
+const isValid = User.validateUser(username, email);
 
-console.log('is Valid => ', isValid)
+console.log("is Valid => ", isValid);
 
 if (isValid) {
-  const newUser = new User(username, email)
-  console.log(' ✅ NEW USER CREATED =>', newUser)
+  const newUser = new User(username, email);
+  console.log(" ✅ NEW USER CREATED =>", newUser);
 }
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+const Car = function () {
+  this.drive = function () { console.log(this) }
+}
+const price = new Car()
+
+price.drive()
