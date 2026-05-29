cb78a4a33a86fe9ed241d26f7f5fe7a24bee9aef
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-05-29 13:14:42 +0330

    STATIC METHODS

--- animals.js
+++ animals.js
@@ -27,3 +27,30 @@ const myDog = new Dog('JACKIE')
 console.log(myDog.speak())
 console.log(myDog.bark())
 
+class User {
+  constructor(username, email) {
+    this.username = username
+    this.email = email
+  }
+
+  static validateUser(username, email) {
+    if (username.trim() == '' || username.length < 4 || email.trim() == '' || !email.includes("@")) {
+      console.log('%c 🛑 USER INFOS ARE NOT VALID ...', 'color :rgb(248, 120, 120)');
+      return false;
+    }
+    console.log('%c ✅ USER INFOS ARE VALID ...', 'color :rgb(120, 248, 152)');
+    return true
+  }
+}
+
+const username = 'pourya'
+const email = 'pourya@gmail.com'
+
+const isValid = User.validateUser(username, email)
+
+console.log('is Valid => ', isValid)
+
+if (isValid) {
+  const newUser = new User(username, email)
+  console.log(' ✅ NEW USER CREATED =>', newUser)
+}
