7e2a1f61a41b9417f6413db49cb798729814e6a5
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-07-17 12:56:47 +0330

    FOR___OF

--- js___review.js
+++ js___review.js
@@ -403,3 +403,10 @@ usersArray.forEach((user, index) => {
       </p>
       `)
 })
+
+// FOR OF 
+const numbers = [1, 2, 3, 4, 5, 6]
+
+for (const number of numbers) {
+  console.log('number =>', number)
+}
